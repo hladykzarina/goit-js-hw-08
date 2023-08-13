cfd5c79841ea1bd00762a362edf55f5ec79a833f
bogunenko-daryna <124713486+hladykzarina@users.noreply.github.com>
2023-08-13 15:57:08 +0200

    first commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,6 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-import 'simplelightbox/dist/simple-lightbox.min.css';
 
 //Сделать рефакторинг
 const list = document.querySelector('.gallery');
